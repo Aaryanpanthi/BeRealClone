aaef3a5f1153f5501c846d84d396079f6ba84c79
Aaryan1604 <aryanpanthi3@gmail.com>
2026-02-02 21:55:53 -0500

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Aaryan Panthi on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
